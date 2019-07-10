b9a4f7ba450699b6f3df1f16a8ddb9d2bfa05b0c
Jesse Blanton <jblanton.eng@gmail.com>
2019-07-10 19:59:56 -0400

    add temperature conversion with F as defaut

--- 13-Capstone/Capstone.Web/Controllers/HomeController.cs
+++ 13-Capstone/Capstone.Web/Controllers/HomeController.cs
@@ -25,10 +25,25 @@ namespace Capstone.Web.Controllers
         public IActionResult Detail(string parkCode)
         {
             dynamic mymodel = new ExpandoObject();
+
+            if(HttpContext.Session.GetString("TemperatureUnit") == null)
+            {
+                HttpContext.Session.SetString("TemperatureUnit", "F");
+            }
+            
+            ViewBag.TemperatureUnit = HttpContext.Session.GetString("TemperatureUnit");
+
             mymodel.Forecast = forecastDAO.GetWeatherByParkCode(parkCode);
             mymodel.Park = parkDAO.GetParkByParkCode(parkCode);
 
             return View(mymodel);
+        }
+
+        public IActionResult ChangeTemperatureUnits(string unit, string parkCode)
+        {
+            HttpContext.Session.SetString("TemperatureUnit", unit);
+
+            return RedirectToAction("Detail", new { parkCode });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
