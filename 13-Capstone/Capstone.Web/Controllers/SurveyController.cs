ebae6e08d05ad692e4c18d28a60c22d6bd3e000e
Jesse Blanton <jblanton.eng@gmail.com>
2019-07-10 15:21:56 -0400

    add code to use park and state lists in survey view

--- 13-Capstone/Capstone.Web/Controllers/SurveyController.cs
+++ 13-Capstone/Capstone.Web/Controllers/SurveyController.cs
@@ -6,23 +6,26 @@ using System.Threading.Tasks;
 using Capstone.Web.DAL;
 using Capstone.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
 
 namespace Capstone.Web.Controllers
 {
-    public class SurveyController : Controller
+    public class SurveyController : HomeController
     {
         private ISurveyDAO surveyDAO;
+        private IParkDAO parkDAO;
 
-        public SurveyController(ISurveyDAO surveyDAO)
+        public SurveyController(ISurveyDAO surveyDAO, IParkDAO parkDAO)
         {
             this.surveyDAO = surveyDAO;
+            this.parkDAO = parkDAO;
         }
 
         [HttpGet]
         public IActionResult Survey()
         {
-            // todo get list of park names for dropdown menu
-            // todo get list of states for dropdown menu
+            ViewBag.Parks = parkDAO.GetParks();
+            ViewBag.States = states;
 
             return View();
         }
@@ -51,5 +54,61 @@ namespace Capstone.Web.Controllers
             
             return View(parks);
         }
+
+        public List<string> states = new List<string>
+        {
+                {"AK - Alaska" },
+                {"AL - Alabama" },
+                {"AR - Arkansas" },
+                {"AZ - Arizona" },
+                {"CA - California" },
+                {"CO - Colorado" },
+                {"CT - Connecticut" },
+                {"DC - District of Columbia" },
+                {"DE - Delaware" },
+                {"FL - Florida" },
+                {"GA - Georgia" },
+                {"HI - Hawaii" },
+                {"IA - Iowa" },
+                {"ID - Idaho" },
+                {"IL - Illinois" },
+                {"IN - Indiana" },
+                {"KS - Kansas" },
+                {"KY - Kentucky" },
+                {"LA - Louisiana" },
+                {"MA - Massachusetts" },
+                {"MD - Maryland" },
+                {"ME - Maine" },
+                {"MI - Michigan" },
+                {"MN - Minnesota" },
+                {"MO - Missouri" },
+                {"MS - Mississippi" },
+                {"MT - Montana" },
+                {"NC - North Carolina" },
+                {"ND - North Dakota" },
+                {"NE - Nebraska" },
+                {"NH - New Hampshire" },
+                {"NJ - New Jersey" },
+                {"NM - New Mexico" },
+                {"NV - Nevada" },
+                {"NY - New York" },
+                {"OH - Ohio" },
+                {"OK - Oklahoma" },
+                {"OR - Oregon" },
+                {"PA - Pennsylvania" },
+                {"PR - Puerto Rico" },
+                {"RI - Rhode Island" },
+                {"SC - South Carolina" },
+                {"SD - South Dakota" },
+                {"TN - Tennessee" },
+                {"TX - Texas" },
+                {"UT - Utah" },
+                {"VA - Virginia" },
+                {"VT - Vermont" },
+                {"WA - Washington" },
+                {"WI - Wisconsin" },
+                {"WV - West Virginia" },
+                {"WY - Wyoming" }
+        };
     }
 }
